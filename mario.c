c4865cda314f9090a1845db8b457c95ae72d2078
redyelruc <redyelruc@users.noreply.github.com>
2020-05-03 22:35:48 +0000

    automated commit by check50 [check50=True]

--- mario.c
+++ mario.c
@@ -4,20 +4,26 @@
 
 int main(void)
 {
-    int height = 0;  // declares an int variable called height
-    do    //runs a loop to ensure that the user input is above zero and below 9
+    int height= 0;  
+    // declares an int variable called height
+    do    
+    //runs a loop to ensure that the user input is above zero and below 9
     {
-        height = get_int("Height: "); // asks user for height of pyramid
+        height = get_int("Height: "); 
+        // asks user for height of pyramid
     }
-    while(height <= 0 || height >8);
+    while (height <= 0 || height > 8);
 
-    int row, column;  // declares int variables to count through loops
+    int row, column;  
+    // declares int variables to count through loops
 
-    for(row=1;row<=height;row++)  // loops through each row of the pattern
+    for (row = 1; row <= height; row++)  
+    // loops through each row of the pattern
     {
-        for(column=0;column<height;column++)  // prints a character for each column on the left half
+        for (column = 0; column < height; column++)  
+        // prints a character for each column on the left half
         {
-            if(column<(height-row))
+            if (column < (height - row))
             {
                 printf(" ");
             }
@@ -28,12 +34,14 @@ int main(void)
 
         }
 
-        printf("  "); //prints the space between the two sides
+        printf("  "); 
+        //prints the space between the two sides
 
-        for(column=0;column<row;column++)    // prints a character for each column on the right half
+        for (column = 0; column < row; column++)    
+        // prints a character for each column on the right half
         {
             printf("#");
         }
-    printf("\n");   //passes to a new line
+        printf("\n");   //passes to a new line
     }
 }
