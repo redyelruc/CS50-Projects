980fda78dfce704ae837057cb60668acbb6628af
redyelruc <redyelruc@users.noreply.github.com>
2020-05-03 22:39:05 +0000

    automated commit by submit50

--- mario.c
+++ mario.c
@@ -7,7 +7,7 @@ int main(void)
     int height = 0;  
     // declares an int variable called height
     do    
-    //runs a loop to ensure that the user input is above zero and below 9
+        //runs a loop to ensure that the user input is above zero and below 9
     {
         height = get_int("Height: "); 
         // asks user for height of pyramid
@@ -18,10 +18,10 @@ int main(void)
     // declares int variables to count through loops
 
     for (row = 1; row <= height; row++)  
-    // loops through each row of the pattern
+        // loops through each row of the pattern
     {
         for (column = 0; column < height; column++)  
-        // prints a character for each column on the left half
+            // prints a character for each column on the left half
         {
             if (column < (height - row))
             {
@@ -38,10 +38,11 @@ int main(void)
         //prints the space between the two sides
 
         for (column = 0; column < row; column++)    
-        // prints a character for each column on the right half
+            // prints a character for each column on the right half
         {
             printf("#");
         }
-        printf("\n");   //passes to a new line
+        printf("\n");   
+        //passes to a new line
     }
 }
