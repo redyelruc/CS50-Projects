c4695fccef4f07cf5c6d8cc8d417bd92a98ad52d
redyelruc <redyelruc@users.noreply.github.com>
2020-05-03 20:24:29 +0000

    automated commit by submit50

--- hello.c
+++ hello.c
@@ -3,6 +3,6 @@
 
 int main(void)
 {
-    string name = get_string("What is your name?\n");
-    printf("hello, %s.\n", name);
+    string name = get_string("What is your name?\n"); //decares a variable name of the string type and asks the user to input a string
+    printf("hello, %s.\n", name);  // prints hello + name
 }
