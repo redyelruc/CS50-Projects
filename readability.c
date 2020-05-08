266e456dd5940f4ef05953a0502a7ccfa3132c81
redyelruc <redyelruc@users.noreply.github.com>
2020-05-08 17:01:41 +0000

    automated commit by check50 [check50=True]

---
+++ readability.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+#include <math.h>
+
+int main(void)
+{
+    int words = 0;
+    int letters = 0;
+    int sentences = 0;
+    string text;
+    float letters_per_hundred = 0.0;
+    float sentences_per_hundred = 0.0;
+    float cole_liau = 0.0;
+    
+    text = get_string("Text: ");
+    for (int i=0;i<strlen(text); i++)
+    {
+        if (text[i] == 33 ||  text[i] == 46 ||  text[i] == 63)
+        {
+            sentences ++;
+        }
+        if (text[i] == 32)
+        {
+            words++;
+        }
+        if ((text[i] >= 65 && text[i] <= 90)  || (text[i] >= 97 && text [i] <= 122))
+        {
+            letters++;
+        }
+    }
+    words++;
+    printf ("%d letter(s)\n", letters);
+    printf ("%d word(s)\n", words);
+    printf ("%d sentence(s)\n", sentences);
+    
+    letters_per_hundred = 100 * (float)letters/words;
+    sentences_per_hundred = 100 * (float)sentences/words;
+    cole_liau = 0.0588 * letters_per_hundred - 0.296 * sentences_per_hundred - 15.8;
+    cole_liau = round(cole_liau);
+    if (cole_liau < 1)
+    {
+    	printf ("Before grade 1\n");
+    }
+    else if (cole_liau > 16)
+    {
+    	printf ("Greater than grade 16+\n");
+    }
+    else
+    {
+    	printf ("Grade %d\n", (int)cole_liau);
+    }
+}
