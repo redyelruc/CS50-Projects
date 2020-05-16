51b5d75ec251220336e7dc77402c7a6b3c5484d8
redyelruc <redyelruc@users.noreply.github.com>
2020-05-16 14:07:28 +0000

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -11,12 +11,9 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
         {
             float average = (float)(image[row][column].rgbtBlue + image[row][column].rgbtGreen + image[row][column].rgbtRed)/3;
             int rounded_average = (int)round(average);
-            // printf("%x,%x,%x,%x", image[0][0].rgbtBlue, image[0][0].rgbtGreen, image[0][0].rgbtRed, rounded_average);
-            
             image[row][column].rgbtBlue = rounded_average;
             image[row][column].rgbtGreen = rounded_average;
             image[row][column].rgbtRed = rounded_average;
-            // printf("%x,%x,%x", image[0][0].rgbtBlue, image[0][0].rgbtGreen, image[0][0].rgbtRed);
         }
     }
     return;
@@ -25,7 +22,19 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
-    return;
+    for (int row = 0; row < height; row++)
+    {
+        for (int column = 0; column < width; column ++)
+        {
+            float sepia_red = .393 * image[row][column].rgbtRed + .769 * image[row][column].rgbtGreen + .189 * image[row][column].rgbtBlue;
+            float sepia_green = .349 * image[row][column].rgbtRed + .686 * image[row][column].rgbtGreen + .168 * image[row][column].rgbtBlue;
+            float sepia_blue = .272 * image[row][column].rgbtRed + .534 * image[row][column].rgbtGreen + .131 * image[row][column].rgbtBlue;
+            image[row][column].rgbtBlue = (int)round(sepia_blue);
+            image[row][column].rgbtGreen = (int)round(sepia_green);
+            image[row][column].rgbtRed = (int)round(sepia_red);
+        }
+    }
+    return;return;
 }
 
 // Reflect image horizontally
